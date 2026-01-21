30127a84135fc753857c5ddaa6b17ff0770a42ba
Premier Home <contatopremierhome@gmail.com>
2026-01-21 01:11:34 +0000

    coloque esse pixel na minha : 902096692069775

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { WhatsappButton } from '@/components/landing/WhatsappButton';
@@ -24,8 +25,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={inter.variable}>
-      <head />
+      <head>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '902096692069775');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body className="font-body antialiased">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=902096692069775&ev=PageView&noscript=1"/>`,
+          }}
+        />
         {children}
         <Toaster />
         <WhatsappButton />
